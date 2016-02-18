541af20ee5b5483f366be0956abfb68669f88c0d
kristofliesenborghs <liesenborghs.kristof@gmail.com>
2016-02-18 13:27:15 +0100

    BroodCode mapstructuur

---
+++ tester.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <td>
+                    <img src='http://barcode.tec-it.com/barcode.ashx?data=1&code=Code128&dpi=96' alt='Barcode Generator TEC-IT'/>
+                </td>
+            </tr>
+        </table>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
